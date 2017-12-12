719540d954090ee72cd499f778366c2b9a8f57e5
Felipe Oriani <felipe.oriani@gatec.com.br>
2017-12-12 20:37:28 -0200

    Adicionar Aula 02

---
+++ Treinamento/Aula02/Model/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula02.Model
+{
+    public class Item
+    {
+        public DateTime Data { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Data.ToString("dd-MM-yyyy HH:mm")} - {Valor.ToString("C2")}";
+        }
+    }
+}
